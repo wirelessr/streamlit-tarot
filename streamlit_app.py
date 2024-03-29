4b34d94473ea38c8dcc4af18ac13c37e0b9288fe
Chunting Wu <chunting.wu@langlive.com>
2024-03-29 10:49:29 +0800

    feat: gemini integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,44 @@
 import os
 import random
 import json
+from collections import namedtuple
 import streamlit as st
 from PIL import Image
 import numpy as np
+
+@st.cache_resource
+def init_connection():
+    import pymongo
+    return pymongo.MongoClient(st.secrets["mongo"]["uri"])
+
+client = init_connection()
+
+@st.cache_resource
+def init_embeddings():
+    from langchain_google_genai import GoogleGenerativeAIEmbeddings
+    return GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+@st.cache_resource
+def init_llm():
+    from langchain_google_genai import ChatGoogleGenerativeAI
+    return ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
+
+def init_retriever(client, embeddings):
+    from langchain_community.vectorstores import MongoDBAtlasVectorSearch
+
+    DB_NAME = "langchain_db"
+    COLLECTION_NAME = "test"
+    ATLAS_VECTOR_SEARCH_INDEX_NAME = "index_name"
+    MONGODB_COLLECTION = client[DB_NAME][COLLECTION_NAME]
+
+    vector_search = MongoDBAtlasVectorSearch.from_connection_string(
+        st.secrets["mongo"]["uri"],
+        DB_NAME + "." + COLLECTION_NAME,
+        embeddings,
+        index_name=ATLAS_VECTOR_SEARCH_INDEX_NAME,
+    )
+    return vector_search.as_retriever()
+
 
 @st.cache_data
 def init_desc():
@@ -16,16 +51,17 @@ def init_desc():
 desc, full_desc = init_desc()
 
 # Classic Spreads
-def init_spread():
+SpreadSlot = namedtuple('SpreadSlot', ['card', 'desc'])
+def init_time_flow_spread():
     card_cols = st.columns(3)
     card_cols = [x.empty() for x in card_cols]
 
-    title_cols = st.columns(3)
-    assert len(title_cols) == len(card_cols)
+    desc_cols = st.columns(3)
+    assert len(desc_cols) == len(card_cols)
 
-    return [(card_cols[i], title_cols[i]) for i in range(len(title_cols))]
+    return [SpreadSlot(card_cols[i], desc_cols[i]) for i in range(len(desc_cols))]
 
-spread = init_spread()
+spread = init_time_flow_spread()
 
 base_dir = '.'
 img_dir = os.path.join(base_dir, 'img')
@@ -50,63 +86,75 @@ def open_image(fp):
 
     return image_array, flipped_image_array
 
+CardStat = namedtuple('CardStat', ['img', 'img_name', 'reversed'])
 def pick_one():
     card_path = files.pop()
     img = open_image(os.path.join(card_img_dir, card_path))
     img_idx = random.choice([0, 1])
-    return img[img_idx], card_path
+    return CardStat(img[img_idx], card_path, img_idx == 1)
 
 # config
 with st.sidebar:
+    spread_type = st.selectbox('Using Spread', ['Time Flow'])
     is_full_desc = st.toggle('Full Description')
     is_gemini = st.toggle('Gemini Assistant')
     if is_gemini:
         gemini_token = st.text_input('Gemini Token', type='password')
 
+if spread_type == 'Time Flow':
+    spread = init_time_flow_spread()
+    sys_prompt = '這是一個時間之流的塔羅牌牌陣，抽三張牌分別代表過去、現在和未來，根據使用者的問題、使用者抽出三張牌的意思以及以下{context}，給出一個合理的解釋'
+
 # Init card
-for card, _ in spread:
-    card.image(os.path.join(img_dir, 'cover.png'))
+for slot in spread:
+    slot.card.image(os.path.join(img_dir, 'cover.png'))
 
 if st.button('Go', use_container_width=True):
     st.session_state['cards'] = []
-    for card, title in spread:
-        img, pk = pick_one()
-        st.session_state['cards'].append((img, pk))
+    for _ in spread:
+        stat = pick_one()
+        st.session_state['cards'].append(stat)
 
 
 if 'cards' in st.session_state:
     card_state = st.session_state['cards']
-    for idx, (card, title) in enumerate(spread):
-        card_img = card_state[idx][0]
-        card_pk = card_state[idx][1]
-        card.image(card_img, caption=desc[card_pk]['t'])
+    for idx, slot in enumerate(spread):
+        card_img = card_state[idx].img
+        card_pk = card_state[idx].img_name
+        slot.card.image(card_img, caption=desc[card_pk]['t'])
 
-        title.write(desc[card_pk]['p'])
-        title.write(desc[card_pk]['n'])
+        slot.desc.write(desc[card_pk]['p'])
+        slot.desc.write(desc[card_pk]['n'])
         if is_full_desc:
-            title.text('\n'.join(full_desc[desc[card_pk]['t']]))
+            slot.desc.text('\n'.join(full_desc[desc[card_pk]['t']]))
 
-    if is_gemini:
+    if is_gemini and gemini_token:
+        from langchain.chains.combine_documents import create_stuff_documents_chain
+        from langchain.chains import create_retrieval_chain
+        from langchain_core.prompts import ChatPromptTemplate
+
+        os.environ['GOOGLE_API_KEY'] = gemini_token
+
+        llm = init_llm()
+        embeddings = init_embeddings()
+        retriever = init_retriever(client, embeddings)
+
+        input_text = ', '.join([f"{'逆位' if stat.reversed else ''}{desc[stat.img_name]['t']}" for stat in card_state])
+
+        prompt = ChatPromptTemplate.from_messages([
+            ('system', sys_prompt),
+            ('user', 'Question: {input}'),
+        ])
+        document_chain = create_stuff_documents_chain(llm, prompt)
+        retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+        with st.chat_message("user"):
+            st.markdown(input_text)
+
+        response = retrieval_chain.invoke({
+            'input': input_text,
+            'context': []
+        })
+        
         with st.chat_message("assistant"):
-            st.markdown('''**過去（寶劍七）：**
-
-這張牌代表狡猾、欺騙和背叛。它表明你在過去曾經歷過一段困難時期，你感到自己被欺騙或背叛。你可能感到不信任或懷疑，並且可能對未來持悲觀態度。
-
-**現在（逆位魔術師）：**
-
-逆位的魔術師代表缺乏創造力、動力和行動。它表明你現在可能感到被困住或受限制，無法充分發揮自己的潛力。你可能會感到沮喪或灰心，並可能在努力實現目標方面遇到困難。
-
-**未來（倒吊者）：**
-
-倒吊者代表犧牲、耐心和逆境中的成長。它表明你未來可能會經歷一段挑戰時期，但它也會給你成長和轉變的機會。你可能會被迫重新評估你的價值觀和優先事項，並可能需要做出一些犧牲。然而，這段經歷最終將幫助你成長並變得更加強大。
-
-**綜合解釋：**
-
-這三個牌陣表明，你過去曾經歷過一段困難時期，感到被欺騙或背叛。現在，你可能感到被困住或受限，無法充分發揮自己的潛力。然而，未來會給你帶來成長和轉變的機會，儘管這可能需要一些犧牲和耐心。
-
-**建議：**
-
-* 信任你的直覺並小心那些讓你感到不信任的人。
-* 找到激勵你的方法，並採取行動實現你的目標。
-* 擁抱逆境，因為它們提供了成長和學習的機會。
-* 保持耐心和積極，相信事情最終會好起來。''')
+            st.markdown(response['answer'])
